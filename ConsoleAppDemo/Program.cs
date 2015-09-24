abaf946afd2e26cb5adc1e9a2405ffec017a2401
Freddy Juhel <domaine75@gmail.com>
2015-09-24 15:00:31 +0200

    And process start for each images

--- ConsoleAppDemo/Program.cs
+++ ConsoleAppDemo/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Drawing.Imaging;
+using System.IO;
 using Alx.Web;
 
 namespace ConsoleAppDemo
@@ -13,28 +15,38 @@ namespace ConsoleAppDemo
 
       var device = Devices.Desktop;
       var path = string.Format(@"C:\Temp\website-{0}.png", device);
-      //Screenshot.Save(url, path, ImageFormat.Png, device);
+      Screenshot.Save(url, path, ImageFormat.Png, device);
       Console.WriteLine("Saved " + path);
+      OpenImage(path);
 
       url = "http://www.codeproject.com/Articles/1030283/Neat-Tricks-for-Effortlessly-Formatting-Currency-i";
       path = string.Format(@"C:\Temp\codeProjectArticle1-{0}.png", device);
       Screenshot.Save(url, path, ImageFormat.Png, device);
       Console.WriteLine("Saved " + path);
+      OpenImage(path);
 
       device = Devices.TabletLandscape;
       path = string.Format(@"C:\Temp\website-{0}.png", device);
       Screenshot.Save(url, path, ImageFormat.Png, device);
-
       Console.WriteLine("Saved " + path);
+      OpenImage(path);
 
       device = Devices.PhonePortrait;
       path = string.Format(@"C:\Temp\website-{0}.png", device);
       Screenshot.Save(url, path, ImageFormat.Png, device);
-
       Console.WriteLine("Saved " + path);
+      OpenImage(path);
 
       Console.WriteLine("Press any key to exit");
       Console.ReadKey();
+    }
+
+    private static void OpenImage(string fileName)
+    {
+      if (File.Exists(fileName))
+      {
+        Process.Start(fileName);
+      }
     }
   }
 }
