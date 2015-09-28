9a6bde3717e2dcd4e1a665992e94dcb80cd01ff4
Freddy Juhel <domaine75@gmail.com>
2015-09-28 19:52:55 +0200

    Remove C# 6.0 code

--- Alx.Web/SizeAttribute.cs
+++ Alx.Web/SizeAttribute.cs
@@ -7,12 +7,14 @@ namespace Alx.Web
     public int Width { get; set; }
     public int Height { get; set; }
 
-    public Size Size => new Size(Width, Height);
+    //public Size Size => new Size(Width, Height); // Framework 4.6
+    public Size Size { get; set; }
 
     public SizeAttribute(int width, int height)
     {
       Width = width;
       Height = height;
+      Size = new Size(Width, Height);
     }
   }
 }
