6bbf9e461f6675cc107a8833b0f8c8020ab038e8
fred <domaine75@gmail.com>
2015-10-20 13:42:01 +0200

    Fix utf-8 tag without space

--- WebBrowserRecorder/FormMain.cs
+++ WebBrowserRecorder/FormMain.cs
@@ -132,11 +132,7 @@ namespace WebBrowserRecorder
     {
       List<string> minimumVersion = new List<string>
       {
-        "<?xml version=\"1.0\" encoding=\"utf - 8\" ?>",
-        "<Document>",
-        "<DocumentVersion>",
-        "<version> 1.0 </version>",
-        "</DocumentVersion>",
+        "<?xml version=\"1.0\" encoding=\"utf-8\" ?>",
         "<terms>",
          "<term>",
         "<name>MenuFile</name>",
@@ -268,8 +264,7 @@ namespace WebBrowserRecorder
           "<englishValue>About</englishValue>",
           "<frenchValue>A propos de ...</frenchValue>",
         "</term>",
-        "</terms>",
-        "</Document>"
+        "</terms>"
       };
       StreamWriter sw = new StreamWriter(Settings.Default.LanguageFileName);
       foreach (string item in minimumVersion)
