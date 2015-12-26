e247e8a05053f193c777a70f522a4b6fd8dbbe1a
fred <domaine75@gmail.com>
2015-12-27 00:43:08 +0100

    Update FormMain.cs

--- WebBrowserRecorder/FormMain.cs
+++ WebBrowserRecorder/FormMain.cs
@@ -530,7 +530,7 @@ namespace WebBrowserRecorder
     {
       if (tb != ActiveControl) return;
       var selectionIndex = tb.SelectionStart;
-      tb.Text = tb.Text.Insert(selectionIndex, Clipboard.GetText());
+      tb.SelectedText = Clipboard.GetText();
       tb.SelectionStart = selectionIndex + Clipboard.GetText().Length;
     }
 
